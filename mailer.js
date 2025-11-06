9c7db8c8f98b5c2779fcda758468d5682da3622f
Sanath <sanathmandala391@gmail.com>
2025-11-06 08:09:57 +0530

    H

--- mailer.js
+++ mailer.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 async function sendEmail({ to, subject, html }) {
   return await resend.emails.send({
-    from: "sanathmandala391@gmail.com", // your verified Gmail
+    from: "onboarding@resend.dev", // your verified Gmail
     to,
     subject,
     html,
