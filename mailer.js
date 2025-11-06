bacef831be07a88d2ea1a0767db3a6725d26bb21
Sanath <sanathmandala391@gmail.com>
2025-11-06 07:54:25 +0530

    A

--- mailer.js
+++ mailer.js
@@ -10,15 +10,15 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.zoho.in",
-  port: 465,
-  secure: true, // SSL
+  port: 587,       // use 587 instead of 465
+  secure: false,   // TLS
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    pass: process.env.EMAIL_PASS, // Zoho App Password
   },
+  tls: { ciphers: "SSLv3" },
 });
 
-// Wrap sending in an async function
 async function sendEmail({ to, subject, html }) {
   return await transporter.sendMail({
     from: process.env.EMAIL_USER,
