8a12edfa6bff1cf3bc1bad3d8ea123511a00dc58
sanathmandala391-stack <sanathmandala391@gmail.com>
2026-02-06 11:34:15 +0530

    updated newly

--- index.js
+++ index.js
@@ -100,6 +100,8 @@ app.use(
 );
 
 app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // Static files (Note: Vercel might not persist these; consider Cloudinary/S3 later)
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
