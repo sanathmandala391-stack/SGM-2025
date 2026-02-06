e4672b942820bab5869f120784df16e7da301919
sanathmandala391-stack <sanathmandala391@gmail.com>
2026-02-06 11:48:08 +0530

    DDD

--- routes/timetableRoute.js
+++ routes/timetableRoute.js
@@ -3,7 +3,7 @@ const express=require("express");
 
 const router=express.Router();
 
-router.post("/add-timetable",timetableController.addTimetable);
+router.post("/timetable/add-timetable",timetableController.addTimetable);
 router.get("/gettimetable",timetableController.getTimetable);
 
 module.exports=router;
