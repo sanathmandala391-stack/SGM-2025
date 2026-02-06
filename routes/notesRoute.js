86d618492790b5a7fca4112fb41bd4314803e1d2
sanathmandala391-stack <sanathmandala391@gmail.com>
2026-02-06 12:39:17 +0530

    Xxxx

--- routes/notesRoute.js
+++ routes/notesRoute.js
@@ -1,4 +1,4 @@
-const notesController=require("../controllers/notesContoller");
+/*const notesController=require("../controllers/notesContoller");
 const express=require("express");
 
 const router=express.Router();
@@ -7,3 +7,15 @@ router.post("/notes",notesController.addNotes);
 router.get("/getnotes",notesController.getNotes);
 
 module.exports=router;
+*/
+
+
+const notesController = require("../controllers/notesContoller");
+const express = require("express");
+const router = express.Router();
+
+// Put the middleware [upload.single("file")] here directly
+router.post("/notes", notesController.upload.single("file"), notesController.addNotes);
+router.get("/getnotes", notesController.getNotes);
+
+module.exports = router;
